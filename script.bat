fff6ef9c0208e8f0f878cf18254ab0d6890ecc80
Steven Serrano <serrano1314@gmail.com>
2021-04-16 12:14:10 +0800

    add output.txt

--- script.bat
+++ script.bat
@@ -1,14 +1,20 @@
 @echo off
-set /a n1=0
-set /a n2=1
+set /a na=0
+set /a nb=0
 setlocal enabledelayedexpansion
 for /f "tokens=*" %%a in (backup_path.txt) do (
   set line=%%a
+  pause 
   :loop
-  echo|set /p=^!line:~%n1%,%n2%^!
-  set /a n1=n1+1
-  set /a n2=n2+1
-  echo %n1%-%n2%
-  pause
+  set ch=^!line:~%na%,1^!
+  echo|set /p=!ch!
+  set delim=;
+  set /a na=na+1
+  if !ch!==!delim! (
+    echo ^!line:~%nb%,%na%^! >> output.txt
+    set /a nb=%na%
+    pause
+
+  )
   goto :loop
 )
