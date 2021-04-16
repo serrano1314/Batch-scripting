4d8c97a3e516a7aa6bb9b74baf99405c3fd8a41f
Steven Serrano <serrano1314@gmail.com>
2021-04-16 13:22:59 +0800

    Working script

--- script.bat
+++ script.bat
@@ -1,22 +1,31 @@
 @echo off
 set /a na=0
 set /a nb=0
+set file=backup_path.txt
 setlocal enabledelayedexpansion
-for /f "tokens=*" %%a in (backup_path.txt) do (
-  set line=%%a
-  pause 
+for %%i in (%file%) do @set count=%%~zi
+for /f "tokens=*" %%a in (%file%) do (
+  set line=%%a 
+  ::pause 
   :loop
   set ch=^!line:~%na%,1^!
-  echo|set /p=!ch!
+  ::echo|set /p=!ch!
   set delim=;
+  set end=?
+  if %na% gtr %count% (
+    echo [92mSCRIPTING SUCCESS...[0m
+    pause
+    color 0f
+    goto :endloop
+  )
   set /a na=%na%+1
   if !ch!==!delim! (
     ::echo ^!line:~%nb%,%na%^! >> output.txt
-    echo. >> output.txt
-    set /a nb=%na%
-    pause
+    echo ; >>output.txt
+    echo|set /p=. 
   ) else (
     <nul set /p=!ch!>>output.txt
   )
   goto :loop
 )
+:endloop
