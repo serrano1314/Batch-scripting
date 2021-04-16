8e0eeb3632658af803b4931fb99157d259da8f4e
Steven Serrano <serrano1314@gmail.com>
2021-04-16 10:10:10 +0800

    messed

--- script.bat
+++ script.bat
@@ -3,8 +3,10 @@ set /a n=1
 setlocal enabledelayedexpansion
 for /f "tokens=1-20 delims=;" %%1 in (backup_path.txt) do (
   set /a n=n+1
-  echo|set /p=%%%%%n%
+  echo|set /p=%%%%!n!
   echo.
-  set /a n=%n%+1
-  echo|set /p=%%%%%n%
+  set /a n=n+1
+  set /p n=!n!
+  set /p str=%%%%!n!
+  echo !str!
 )
