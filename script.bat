c9566ee0ae5ccf104a899806df32c3782b51ad5b
Steven Serrano <serrano1314@gmail.com>
2021-04-16 11:09:39 +0800

    remove delims

--- script.bat
+++ script.bat
@@ -1,10 +1,14 @@
 @echo off
-set /a n=1
+set /a n1=0
+set /a n2=1
 setlocal enabledelayedexpansion
-for /f "tokens=1-20 delims=;" %%1 in (backup_path.txt) do (
-  set /a n=n+1
-  echo|set /p=%%%%%n%
-  echo.
-  set /a n=%n%+1
-  echo|set /p=%%%%%n%
+for /f "tokens=*" %%a in (backup_path.txt) do (
+  set line=%%a
+  :loop
+  echo|set /p=^!line:~%n1%,%n2%^!
+  set /a n1=n1+1
+  set /a n2=n2+1
+  echo %n1%-%n2%
+  pause
+  goto :loop
 )
