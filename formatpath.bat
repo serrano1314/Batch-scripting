5a393933e60873c25b134bd6f681e3dd2cd82eeb
stevens phone <anikaanaisha@gmail.com>
2021-04-18 14:46:18 +0800

    remove deleting output.txt fix bug

--- formatpath.bat
+++ formatpath.bat
@@ -1,39 +1,47 @@
 @echo off
-echo.
-echo      Usage: formatpath.bat ^<filename^>
-echo. 
-set /a na=0
-set /a x=0
-set file=%~1
-set outfile=output.txt
-:creatoutfile
-if exist %outfile% ( 
+goto :main
+
+:filename
   set /a x=%x%+1
   set outfile=output%x%.txt
-  goto :creatoutfile
+goto :eof
+
+:main
+  echo.
+  echo      Usage: formatpath.bat ^<filename^>
+  echo. 
+  set /a na=0
+  set /a x=0
+  set file=%~1
+  set outfile=output.txt
+  :creatoutfile
+  if exist %outfile% ( 
+    call :filename
+    goto :creatoutfile
   )
-setlocal enabledelayedexpansion
-for %%i in (%file%) do @set count=%%~zi
-for /f "tokens=*" %%a in (%file%) do (
-  set line=%%a 
-  set delim=;
-  :loop
-  set ch=^!line:~%na%,1^!
-  if %na% gtr %count% (
-    echo.
-    echo [92mSCRIPTING SUCCESS...[0m
-    echo %outfile% file created at,
-    pwd
-    notepad %outfile%
-    goto :endloop
+  setlocal enabledelayedexpansion
+  for %%i in (%file%) do @set count=%%~zi
+  for /f "tokens=*" %%a in (%file%) do (
+    set line=%%a 
+    set delim=;
+    :loop
+    set ch=^!line:~%na%,1^!
+    if %na% gtr %count% (
+      echo.
+      echo [92mSCRIPTING SUCCESS...[0m
+      echo %outfile% file created at,
+      pwd
+      "%outfile%"
+      goto :endloop
+    )
+    set /a na=%na%+1
+    if !ch!==!delim! (
+      echo ;>>output.txt
+      echo|set /p=.
+    ) else (
+      <nul set /p=!ch!>>output.txt
+    )
+    goto :loop
   )
-  set /a na=%na%+1
-  if !ch!==!delim! (
-    echo ;>>output.txt
-    echo|set /p=.
-  ) else (
-    <nul set /p=!ch!>>output.txt
-  )
-  goto :loop
-)
-:endloop
+  :endloop
+goto :eof
